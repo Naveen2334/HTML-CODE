00b82fa31736b60e5d85220f930e7314b1151f59
NAVEEN2334 <naveenkumar16613@gmail.com>
2023-02-15 23:08:52 +0530

    Html code
    Happy share it

---
+++ htmlfolder/script.js
@@ -0,0 +1,61 @@
+
+let user = [
+    {
+        name : "A",
+        age : 18,
+        martial_status : "married",
+        gender : "m",
+    },
+    {
+        name : "B",
+        age : 17,
+        martial_status : "unmarried",
+        gender : "m",
+    },
+    {
+        name : "C",
+        age : 20,
+        martial_status : "married",
+        gender : "f",
+    },
+    {
+        name : "A",
+        age : 18,
+        martial_status : "married",
+        gender : "m",
+    },
+    {
+        name : "A",
+        age : 18,
+        martial_status : "married",
+        gender : "m",
+    },
+    {
+        name : "A",
+        age : 18,
+        martial_status : "married",
+        gender : "m",
+    },
+    {
+        name : "A",
+        age : 18,
+        martial_status : "married",
+        gender : "m",
+    },
+]
+//1.create duplicate array(new_arr).
+// Note : work with duplicate array only.
+//2. print gender of unmarried.
+//3. print name and gender of married whose age is less than 18.
+//4.if age is less than 17 ,then add property to the object to the object
+//()eligible for mairrage  : "false"
+//5. if age is less 17,then add property of the object.
+//{eligible for mairrage :"true"}
+//6.In  object,gender is mentioned as 'm' and 'f'    ->
+//change "m" to "male".
+//change "f" to "female"
+//7.Add  a function to every object such that , whenever we call the function it should print updated gender
+//("male" and "female") and eligible for mairrage and name.
+//8. delete object and 3rd  index.
+//9. update the original array with all new properties
+//10.print the data of each object. 
